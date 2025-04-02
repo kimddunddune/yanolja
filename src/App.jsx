769b2948f4da07435f4c6950ad77c2a048b682ef
kimddunddune <mungduya09@gmail.com>
2025-04-02 12:48:48 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter } from 'react-router-dom'
+import Header from './component/Header'
+import Menu from './component/Menu'
+import Slide from './component/Slide'
+import Tab from './component/Tab'
+import Footer from './component/Footer'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <BrowserRouter>
+      <div id="wrap">
+        <Header />
+        <Menu />
+        <Slide />
+        <Tab />
+        <Footer />
+      </div>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
